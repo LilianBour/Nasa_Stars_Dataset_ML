562fe0ab6363174aa19664adaa9a56ff4812f45c
Lilian <lilianbour0@gmail.com>
2021-10-16 13:04:57 +0200

    Added Kmeans, Knn, OnevsAll, Naive Bayes, Conclusion

--- Data_Exploration.py
+++ Data_Exploration.py
@@ -9,43 +9,6 @@ df = pd.read_csv("Stars.csv")
 
 print(df.head)
 
-
-#Replace same color written differently
-df['Color'] = df['Color'].str.replace('Blue white','Blue White')
-df['Color'] = df['Color'].str.replace('Blue-White','Blue White')
-df['Color'] = df['Color'].str.replace('Blue-white','Blue White')
-df['Color'] = df['Color'].str.replace('white','White',)
-df['Color'] = df['Color'].str.replace('yellowish', 'Yellowish')
-df['Color'] = df['Color'].str.replace('yellow-White','White Yellow')
-df['Color'] = df['Color'].str.replace('White-yellow','White Yellow')
-
-#Format color names
-df['Color'] = df['Color'].str.replace('White-Yellow','White Yellow')
-df['Color'] = df['Color'].str.replace('Orange-Red','Orange Red')
-df['Color'] = df['Color'].str.replace('Pale yellow orange','Pale Yellow Orange')
-
-#Some categories such as Orange/Orange Red/Pale Yellow Orange have only a few stars, maybe they should be fused with other star colours
-#Orange/Orange Red/Pale Yellow Orange
-#White, White Yellow, Whitish, Yellowish, Yellowish White
-#KEEP IDEA FOR LATER
-"""
-#Chart Number of Stars per Colour
-labels = df['Color'].unique()
-count = df.groupby('Color').count().iloc[:,1]
-print(count)
-explode = [0.0001]*len(labels)  # only "explode" the 2nd slice (i.e. 'Hogs')
-colours=['firebrick','lightblue','gainsboro','coral','moccasin','royalblue','darkgray','gold','orange','yellow','orangered']
-
-plt.pie(count, colors=colours,explode=explode, autopct=lambda p :'{:.0f}'.format(p * sum(count) / 100))
-plt.legend(labels=labels,bbox_to_anchor=(0.9,0.5), loc="center right", fontsize=15,bbox_transform=plt.gcf().transFigure)
-plt.title('Number of Stars per Colour',fontsize=20)
-plt.show()
-
-#Box plot to observe temperature range
-sns.boxplot(data=df, x=df['Color'], y=df['Temperature'])
-plt.show()
-"""
-
 #Chart Number of Stars per Type
 labels = df['Type'].unique()
 count = df.groupby('Type').count().iloc[:,1]
@@ -82,13 +45,14 @@ sns.boxplot(data=df, x=df['Type'], y=df['A_M'])
 for col in df.columns:
   df[col]=df[col].astype('category').cat.codes
 
-"""
-#PCA
+
+
+#---PCA---
 #Separating target and results
 x=df.loc[:,df.columns != 'Type']
 y=df.loc[:,['Type']]
 
-# Standardizing the features
+#Standardizing the features
 x = StandardScaler().fit_transform(x)
 
 from sklearn.decomposition import PCA
@@ -109,31 +73,20 @@ for target, color in zip(targets,colors):
     ax.scatter(finalDf.loc[indicesToKeep, 'principal component 2'], finalDf.loc[indicesToKeep, 'principal component 1'], c = color, s = 50)
 ax.legend(targets)
 plt.show()
-#TODO Importance of each features
-"""
 
-#TSNE
+
+#---TSNE---
 x_tsne=df.loc[:,df.columns != 'Type']
 y_tsne=df.loc[:,['Type']]
 
 tsne = TSNE()
 X_embedded = tsne.fit_transform(x_tsne)
 
-#data = {'tsne-2d-one':X_embedded[:,0],'tsne-2d-two':X_embedded[:,1],'y':y_tsne}
-#df_subset = pd.DataFrame(data)
-
 df_subset = pd.DataFrame(columns=['tsne-2d-one','tsne-2d-two','y'])
 df_subset['tsne-2d-one']=X_embedded[:,0]
 df_subset['tsne-2d-two']=X_embedded[:,1]
 df_subset['y']=y_tsne
 
-
 plt.figure(figsize=(16,10))
-sns.scatterplot(
-    x="tsne-2d-two", y="tsne-2d-one",
-    hue=df_subset.y.tolist(),
-    palette=sns.color_palette("hls", 6),
-    data=df_subset,
-    legend="full",
-)
+sns.scatterplot(x="tsne-2d-two", y="tsne-2d-one",hue=df_subset.y.tolist(),palette=sns.color_palette("hls", 6),data=df_subset,legend="full")
 plt.show()
--- Machine_Learning.py
+++ Machine_Learning.py
@@ -1,13 +1,16 @@
-#KNN - Supervised Learning
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import accuracy_score
-
+from sklearn.decomposition import PCA
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 
 df = pd.read_csv("Stars.csv")
 #Set A_M (Manitude) and Spectral_Class to numerical values
@@ -18,6 +21,7 @@ for col in df.columns:
 X=df.loc[:,df.columns != 'Type']
 y=df.loc[:,['Type']]
 
+#---KNN---
 #Create Train Val and Test (60/20/20)
 X_T, X_test, y_T, y_test = train_test_split(X, y, test_size=0.20)
 X_train, X_val, y_train, y_val = train_test_split(X_T, y_T, test_size=0.25)
@@ -29,11 +33,10 @@ X_train = scaler.transform(X_train)
 X_val = scaler.transform(X_val)
 X_test = scaler.transform(X_test)
 
-
+#Find best k
 best_k=0
 best_acc=0
 accs = []
-# Calculating error for K values between 1 and 20
 for j in range(1, 20):
     knn = KNeighborsClassifier(n_neighbors=j)
     knn.fit(X_train, y_train)
@@ -60,6 +63,81 @@ y_pred = classifier.predict(X_test)
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
-#Kmeans
 
-#Hierarchical Clustering 
+
+
+#---OnevsAll---
+X_T, X_test, y_T, y_test = train_test_split(X, y, test_size=0.20)
+X_train, X_val, y_train, y_val = train_test_split(X_T, y_T, test_size=0.25)
+Multiclass_model = LogisticRegression(multi_class='ovr')
+Multiclass_model.fit(X_train, y_train)
+
+y_pred = Multiclass_model.predict(X_val)
+print(confusion_matrix(y_val, y_pred))
+print(classification_report(y_val, y_pred))
+
+y_pred = Multiclass_model.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+
+#---Naive Bayes---
+X_T, X_test, y_T, y_test = train_test_split(X, y, test_size=0.20)
+X_train, X_val, y_train, y_val = train_test_split(X_T, y_T, test_size=0.25)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+X_val = sc.transform(X_val)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+y_pred  =  classifier.predict(X_val)
+print(confusion_matrix(y_val, y_pred))
+print(classification_report(y_val, y_pred))
+
+y_pred  =  classifier.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+
+#---Kmeans---
+#Find K
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+#Kmeans
+kmeans = KMeans(n_clusters=6)
+kmeans.fit(X)
+
+#Plot truth
+
+#Plot Predicted
+nb_groupe = 6
+#Load Data
+#Transform the data
+X = StandardScaler().fit_transform(X)
+pca = PCA(n_components=2)
+df = pca.fit_transform(X)
+#Initialize the class object
+kmeans = KMeans(n_clusters= nb_groupe)
+#predict the labels of clusters.
+label = kmeans.fit_predict(df)
+#Getting unique labels
+u_labels = np.unique(label)
+#plotting the results:
+for i in u_labels:
+    plt.scatter(df[label == i , 1] , df[label == i , 0] , label = i)
+plt.title('Predicted')
+plt.legend()
+plt.show()
